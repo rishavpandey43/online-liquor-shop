bcd79e6f1de41694d1844cecb651969f34d02727
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2020-05-21 08:36:00 +0530

    :zap: change the baseUrl to deployed one in delivery app

--- DeliveryApp/src/utils/constant.js
+++ DeliveryApp/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const baseUrl = 'http://192.168.43.240:5555';
-// export const baseUrl = 'https://e-madhushala-backend.herokuapp.com';
+// export const baseUrl = 'http://192.168.43.240:5555';
+export const baseUrl = 'https://e-madhushala-backend.herokuapp.com';
 
 export const authTokenName = 'eSamagri_delivery_auth_token';
 
